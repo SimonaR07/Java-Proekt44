9b390664339f327c203817e55f2f3d281a03a7e6
SimonaR07 <sristomanova32@gmail.com>
2024-01-19 21:13:42 +0100

    Add new java program

---
+++ src/MyClass1.java
@@ -0,0 +1,11 @@
+
+public class MyClass1 {
+	 public static void main(String[] args) {
+	       
+	        Vehicle myVehicle = new Vehicle();
+
+	        myVehicle.horn();
+	    }
+
+
+}
---
+++ src/Vehicle1.java
@@ -0,0 +1,8 @@
+
+public class Vehicle1 {
+	  public void horn() {
+	        System.out.println("BEEP");
+	    }
+	
+
+}
